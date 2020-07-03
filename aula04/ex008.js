48fe3910ca7cd42fbc83b832907a2fdf93401c88
Izaélita Campelo <67238328+Izaelita-campelo@users.noreply.github.com>
2020-07-03 00:21:47 -0300

    comecei o estudos de condição com if

---
+++ aula04/ex008.js
@@ -0,0 +1,7 @@
+var vel = 30.5
+console.log(`A velocidade do seu carro é ${vel}km/h`)
+if(vel > 60) { //condição simples
+    console.log(" Voce ultrapassou a velocidade MULTADO.")
+}
+
+console.log(" Por favor dirija com cuidado!")
---
+++ aula04/ex009.js
@@ -0,0 +1,6 @@
+var país = "EUA"
+if(país == "Brasil"){
+    console.log("Voce é estrangeiro")
+}else{
+    console.log("Voce é Brasileiro")
+}
