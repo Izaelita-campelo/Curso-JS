a8dd0faf4a5fb8e66d4d109ff545afbf329fdd45
Izaélita Campelo <67238328+Izaelita-campelo@users.noreply.github.com>
2020-07-07 10:50:47 -0300

    Estrutura de repetição com: while/do e for

---
+++ aula07/ex014.js
@@ -0,0 +1,15 @@
+// ESTRUTURA DE REPETIÇÃO
+
+//repetição com teste logico no final
+var c = 1
+do{
+    console.log(`Ola pessoa de numero ${c}`)
+    c++ // é o mesmo que c = c +1
+}while(c < 8)
+
+//repetição com teste logico no inicio
+/*var c = 1
+while( c <= 15){
+    console.log(`Ola pessoa de numero ${c}`)
+    c++ // é o mesmo que c = c +1
+}*/
